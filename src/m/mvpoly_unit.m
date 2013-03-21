36b97168faec565256712111090cfd6834dadadc
J.J. Green <j.j.green@gmx.co.uk>
2013-03-21 00:19:43 +0000

    improved test harness, point evaluation test stengthend
    This the start of a change to the recursive Horner
    evaluation, we will require that the first dimension
    of x should range over the variables of p (rather than
    the last).  To start we modify the point-evaluation
    test to use a column vector x (and it now fails)

--- src/m/mvpoly_unit.m
+++ src/m/mvpoly_unit.m
@@ -1,11 +1,14 @@
 % simple octave/matlab test harness 
 
+pattern = 'evaluate';
+
+warning('error', 'Octave:broadcast');
 unit_path = mfilename('fullpath');
 addpath(unit_path);
 
 err = 0;
 
-tfiles = dir(strcat(unit_path,'/','test_*.m'));
+tfiles = dir(strcat(unit_path, '/', 'test_*', pattern, '.m'));
 ntfiles = length(tfiles);
 
 for i = 1:ntfiles
@@ -17,12 +20,17 @@ for i = 1:ntfiles
         feval(tfun);
     catch
         fprintf('%s: %s\n', tfun, lasterr);
+        L = lasterror;
+        S = L.stack;
+        for i = 1:numel(S)
+            Si = S(i);
+            fprintf('%s, line %i\n',Si.file, Si.line);
+        end
         err = err+1;
     end
 
 end
     
 rmpath(unit_path);
-
 fprintf('passed %i/%i tests\n', ntfiles-err, ntfiles);
 
--- src/m/mvpoly_unit/test_mvpoly_cube_evaluate.m
+++ src/m/mvpoly_unit/test_mvpoly_cube_evaluate.m
@@ -5,7 +5,7 @@ function test_mvpoly_cube_evaluate()
     p(0, 0) = -1; p(2, 0) = 1; p(0, 2) = 2;
     
     % point evaluation
-    assert(polyval(p, [1,2]) == 8, 'point evaluation'); 
+    assert(polyval(p, [1, 2]') == 8, 'point evaluation'); 
     
     % grid evaluation
     [x,y] = meshgrid(1:2);
