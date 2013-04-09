47d1fde9afa68fb6eedb49e40a5be8fe342aad24
J.J. Green <j.j.green@gmx.co.uk>
2013-04-10 00:22:55 +0100

    addition with one summand a scalar

--- src/m/@mvpoly_cube/plus.m
+++ src/m/@mvpoly_cube/plus.m
@@ -7,10 +7,24 @@ function q = plus(p1, p2)
 %
 % (equivalently c = a+b) returns the sum of the polynomials
 % a and b.  The summmands need not be of the same size nor
-% of the same number of varaibles
+% of the same number of variables.  One of the arguments may
+% be a scalar, and will then be interpreted as one would 
+% expect
 %
 % See also @mvpoly_cube/minus, @mvpoly_cube/uminus
     
-    q = mvpoly_cube(pluspad(p1.coef, p2.coef));    
+    if isa(p1, 'mvpoly_cube')
+        c1 = p1.coef;
+    else
+        c1 = p1;
+    end
+
+    if isa(p2, 'mvpoly_cube')
+        c2 = p2.coef;
+    else
+        c2 = p2;
+    end
+    
+    q = mvpoly_cube(pluspad(c1, c2));    
 
 end
--- src/m/mvpoly_unit/test_mvpoly_cube_addition.m
+++ src/m/mvpoly_unit/test_mvpoly_cube_addition.m
@@ -9,6 +9,14 @@ function test_mvpoly_cube_addition()
     assert(isa(r,'mvpoly_cube'), 'class for simple sum');
     assert(array_equal(r.coef, [0, 1 ; 1, 0]), 'simple sum');
 
+    r = p + q + 1;
+    assert(isa(r,'mvpoly_cube'), 'class for poly plus scalar');
+    assert(array_equal(r.coef, [1, 1 ; 1, 0]), 'poly plus scalar');
+
+    r = 1 + p + q;
+    assert(isa(r,'mvpoly_cube'), 'class for scalar plus poly');
+    assert(array_equal(r.coef, [1, 1 ; 1, 0]), 'scalar plus poly');
+    
     p = mvpoly_cube(ones(2, 2, 2)) + mvpoly_cube(ones(3, 3));
     assert(array_equal(size(p.coef), [3 3 2]), 'sum of different sizes');
 
